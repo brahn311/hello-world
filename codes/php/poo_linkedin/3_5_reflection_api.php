b2b9b76a2919fb0adcceeeaea297432f2ecff78d
Abrahan Omana <abrahan.omana@gmail.com>
2019-02-21 16:09:07 -0400

    update info

--- codes/php/poo_linkedin/3_5_reflection_api.php
+++ codes/php/poo_linkedin/3_5_reflection_api.php
@@ -25,5 +25,18 @@ class Cat extends Animal {
   }
 }
 
-$reflection_class = new ReflectionClass('Dog'); 
+$reflection_class = new ReflectionClass('Dog');
+
+echo $reflection_class->getName() . "\n";
+var_dump($reflection_class->isAbstract());
+var_dump($reflection_class->isInstantiable());
+var_dump($reflection_class->isInternal());
+
+// tambien podemos ver el comportamiento de metodos y los parametros que reciben
+// puede ser util para:
+// cuando generamos clases dinamicas
+// cuando diseñamos una suite de pruebas podemos probar los metodo de una clase de manera generica
+
+// pueder investigar buscar mas usos Reflection API en el manual de referencia
+
 ?>
